ba5e47cf0441124cf9e6470c31d561daa4857879
anaserrami <ramianas314@gmail.com>
2025-01-05 18:54:24 +0100

    add the commands

---
+++ src/main/java/com/errami/comptecqrses/commonApi/commands/commands.kt
@@ -0,0 +1,28 @@
+package com.errami.comptecqrses.commonApi.commands
+
+import com.errami.comptecqrses.commonApi.enums.AccountStatus
+import org.axonframework.modelling.command.TargetAggregateIdentifier
+
+
+abstract class BaseCommand<T>(
+    @TargetAggregateIdentifier
+    open val id: T
+)
+
+data class CreateAccountCommand(
+    override val id: String,
+    val balance: Double,
+    val currency: String,
+    val status: AccountStatus
+) : BaseCommand<String>(id)
+
+data class UpdateAccountCommand(
+    override val id: String,
+    val balance: Double
+) : BaseCommand<String>(id)
+
+data class CreditAccountCommand(
+    override val id: String,
+    val amount: Double,
+    val currency: String
+) : BaseCommand<String>(id)
